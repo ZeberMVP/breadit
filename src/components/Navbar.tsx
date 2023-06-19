e2e2fd5981b663a62c48af3b9b39372e772bbcbd
ZeberMVP <rubeen722@gmail.com>
2023-06-19 12:58:46 +0200

    feat: add user menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Icons } from "@/components/Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -20,7 +21,7 @@ const Navbar = async () => {
         {/* search bar */}
 
         {session ? (
-          <p>youre logged in</p>
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="/sign-in" className={buttonVariants()}>
             Sign In
