13c580bed59b2d61f185d7495d32e68075831135
ZeberMVP <rubeen722@gmail.com>
2023-06-19 12:25:36 +0200

    feat: sign in user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
 
 export const metadata = {
   title: "Breadit",
@@ -29,6 +30,8 @@ export default function RootLayout({
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
+
+        <Toaster />
       </body>
     </html>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { Icons } from "@/components/Icons";
+import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2 ">
@@ -14,6 +18,14 @@ const Navbar = () => {
         </Link>
 
         {/* search bar */}
+
+        {session ? (
+          <p>youre logged in</p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
