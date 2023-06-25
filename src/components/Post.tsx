5ebc62dfc17fa4dd9ac3325fbc8772139d719889
ZeberMVP <rubeen722@gmail.com>
2023-06-25 18:40:40 +0200

    feat: change username

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -28,54 +28,54 @@ const Post: FC<PostProps> = ({
 	const pRef = useRef<HTMLDivElement>(null)
 
 	return (
-		<div className="rounded-md bg-white shadow">
-			<div className="px-6 py-4 flex justify-between">
+		<div className='rounded-md bg-white shadow'>
+			<div className='px-6 py-4 flex justify-between'>
 				<PostVoteClient
 					postId={post.id}
 					initialVote={currentVote?.type}
 					initialVotesAmt={votesAmt}
 				/>
 
-				<div className="w-0 flex-1">
-					<div className="max-h-40 mt-1 text-xs text-gray-500">
+				<div className='w-0 flex-1'>
+					<div className='max-h-40 mt-1 text-xs text-gray-500'>
 						{subredditName ? (
 							<>
 								<a
-									className="underline text-zinc-900 text-sm underline-offet-2"
+									className='underline text-zinc-900 text-sm underline-offet-2'
 									href={`/r/${subredditName}`}
 								>
 									r/{subredditName}
 								</a>
-								<span className="px-1">•</span>
+								<span className='px-1'>•</span>
 							</>
 						) : null}
-						<span>Posted by u/{post.author.name}</span>{' '}
+						<span>Posted by u/{post.author.username}</span>{' '}
 						{formatTimeToNow(new Date(post.createdAt))}
 					</div>
 
 					<a href={`/r/${subredditName}/post/${post.id}`}>
-						<h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
+						<h1 className='text-lg font-semibold py-2 leading-6 text-gray-900'>
 							{post.title}
 						</h1>
 					</a>
 					<div
-						className="relative text-sm max-h-40 w-full overflow-clip"
+						className='relative text-sm max-h-40 w-full overflow-clip'
 						ref={pRef}
 					>
 						<EditorOutput content={post.content} />
 						{pRef.current?.clientHeight === 160 ? (
-							<div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent" />
+							<div className='absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent' />
 						) : null}
 					</div>
 				</div>
 			</div>
 
-			<div className="bg-gray-50 z-20 text-sm p-4 sm:px-6">
+			<div className='bg-gray-50 z-20 text-sm p-4 sm:px-6'>
 				<a
-					className="w-fit flex items-center gap-2"
+					className='w-fit flex items-center gap-2'
 					href={`/r/${subredditName}/post/${post.id}`}
 				>
-					<MessageSquare className="h-4 w-4" /> {commentAmt} comments
+					<MessageSquare className='h-4 w-4' /> {commentAmt} comments
 				</a>
 			</div>
 		</div>
