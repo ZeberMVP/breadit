655ed137948c1f874e3c0f301357c5e0d9482db8
ZeberMVP <rubeen722@gmail.com>
2023-06-25 19:03:56 +0200

    fix: deploy error

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -29,11 +29,11 @@ const renderers = {
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
 	return (
-		// @ts-expect-error
+		// @ts-ignore
 		<Output
 			data={content}
 			style={style}
-			className="text-sm"
+			className='text-sm'
 			renderers={renderers}
 		/>
 	)
@@ -43,16 +43,16 @@ function CustomImageRenderer({ data }: any) {
 	const src = data.file.url
 
 	return (
-		<div className="relative w-full min-h-[15rem]">
-			<Image alt="image" className="object-contain" fill src={src} />
+		<div className='relative w-full min-h-[15rem]'>
+			<Image alt='image' className='object-contain' fill src={src} />
 		</div>
 	)
 }
 
 function CustomCodeRenderer({ data }: any) {
 	return (
-		<pre className="bg-gray-800 rounded-md p-4">
-			<code className="text-gray-100 text-sm"></code>
+		<pre className='bg-gray-800 rounded-md p-4'>
+			<code className='text-gray-100 text-sm'></code>
 		</pre>
 	)
 }
