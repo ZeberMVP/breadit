dcac8bbf67e2874fa9945cd9d10e65eb32713eb5
ZeberMVP <rubeen722@gmail.com>
2023-06-25 18:47:19 +0200

    feat: close search bar on enter or outside click

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, useCallback, useState } from 'react'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
 import {
 	Command,
 	CommandEmpty,
@@ -12,9 +12,10 @@ import {
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { Prisma, Subreddit } from '@prisma/client'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { Users } from 'lucide-react'
 import debounce from 'lodash.debounce'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
 
 interface SearchBarProps {}
 
@@ -47,9 +48,22 @@ const SearchBar: FC<SearchBarProps> = ({}) => {
 	}, [])
 
 	const router = useRouter()
+	const commandRef = useRef<HTMLDivElement>(null)
+	const pathname = usePathname()
+
+	useOnClickOutside(commandRef, () => {
+		setInput('')
+	})
+
+	useEffect(() => {
+		setInput('')
+	}, [pathname])
 
 	return (
-		<Command className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
+		<Command
+			ref={commandRef}
+			className='relative rounded-lg border max-w-lg z-50 overflow-visible'
+		>
 			<CommandInput
 				value={input}
 				onValueChange={(text) => {
