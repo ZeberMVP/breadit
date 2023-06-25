32f62d5439850a5c7698183468f7944eb68d21de
ZeberMVP <rubeen722@gmail.com>
2023-06-25 19:14:01 +0200

    chore: force dynamic fetch of data at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,29 @@ import { getAuthSession } from '@/lib/auth'
 import { HomeIcon } from 'lucide-react'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
 	const session = await getAuthSession()
 	return (
 		<>
-			<h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
-			<div className="grid grid-cols-1 md:grid:-cols-3 gap-y-4 md:gap-x-4 py-6">
+			<h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
+			<div className='grid grid-cols-1 md:grid:-cols-3 gap-y-4 md:gap-x-4 py-6'>
 				{/* @ts-expect-error server component */}
 				{session ? <CustomFeed /> : <GeneralFeed />}
 
 				{/* subreddit info */}
-				<div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
-					<div className="bg-emereld-100 px-6 py-4">
-						<p className="font-semibold py-3 flex items-center gap-1.5">
-							<HomeIcon className="w-4 h-4" />
+				<div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
+					<div className='bg-emereld-100 px-6 py-4'>
+						<p className='font-semibold py-3 flex items-center gap-1.5'>
+							<HomeIcon className='w-4 h-4' />
 							Home
 						</p>
 					</div>
 
-					<div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-						<div className="flex justify-between gap-x-4 py-3">
-							<p className="text-zinc-500">
+					<div className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
+						<div className='flex justify-between gap-x-4 py-3'>
+							<p className='text-zinc-500'>
 								Your personal Breaddit homepage. Come here to check with your
 								favorite communities.
 							</p>
